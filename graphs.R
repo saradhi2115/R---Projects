e80eb915fe85e6add3a0945aaeedc9a5831753a6
saradhi2115 <54427436+saradhi2115@users.noreply.github.com>
2019-09-13 16:54:24 +0530

    Delete graphs.R

--- graphs.R
+++
@@ -1,8 +0,0 @@
-Histogram
-hist(table_final$Positive, col=rainbow(10))
-hist(table_final$Negative, col=rainbow(10))
-hist(table_final$Score, col=rainbow(10))
-slices <- c(sum(table_final$Positive), sum(table_final$Negative))
-labels <- c("Positive", "Negative")
-library(plotrix)
-pie3D(slices, labels = labels, col=rainbow(length(labels)),explode=0.00, main="positive and negative score in pie chats")
