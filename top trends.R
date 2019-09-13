676edeb9bc0425f63391c16fb8bc6dcbe9a84186
saradhi2115 <54427436+saradhi2115@users.noreply.github.com>
2019-09-13 16:54:52 +0530

    Delete top trends.R

--- top trends.R
+++
@@ -1,9 +0,0 @@
-a_trends = availableTrendLocations()
-woeid = a_trends[which(a_trends$name=="Ottawa"),3]
-canada_trend = getTrends(woeid)
-trends = canada_trend[1:2]
-dat <- cbind(trends$name)
-dat2 <- unlist(strsplit(dat, split=", "))
-dat3 <- grep("dat2", iconv(dat2, "latin1", "ASCII", sub="dat2"))
-dat4 <- dat2[-dat3]
-dat4
